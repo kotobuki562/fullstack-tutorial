73a5f319795f10b430af90d2865598ff6e20b8d6
MaQueen <kotobukitsukasaneko33@docomo.com>
2021-03-01 15:52:04 +0900

    commit

--- start/client/src/cache.ts
+++ start/client/src/cache.ts
@@ -1,3 +1,27 @@
-import { InMemoryCache, Reference } from '@apollo/client';
+import { InMemoryCache, Reference, makeVar } from "@apollo/client";
 
-export const cache: InMemoryCache = new InMemoryCache({});
+export const isLoggedInVar = makeVar<boolean>(!!localStorage.getItem("token"));
+
+// Initializes to an empty array
+export const cartItemsVar = makeVar<string[]>([]);
+export const cache: InMemoryCache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        isLoggedIn: {
+          read() {
+            return isLoggedInVar();
+          },
+        },
+        cartItems: {
+          read() {
+            return cartItemsVar();
+          },
+        },
+        launches: {
+          // ...field policy definitions...
+        },
+      },
+    },
+  },
+});
